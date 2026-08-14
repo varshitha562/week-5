440f35bbb066b1b635cc3d1c3a0b906cf42fba60
Varshitha Poch <varshithapocha@gmail.com>
2026-08-14 12:19:51 +0530

    Add week 5 factorial programs

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+    public class Factorial {
+    public static long compute(int n) {
+        if (n < 0) throw new IllegalArgumentException("Negative number not allowed");
+
+        long result = 1;
+
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Factorial of " + number + " is " + compute(number));
+    }
+}
---
+++ TestFactorial.java
@@ -0,0 +1,17 @@
+public class TestFactorial {
+    public static void main(String[] args) {
+        try {
+            if (Factorial.compute(5) != 120) {
+                throw new AssertionError("Test failed for input 5");
+            }
+            if (Factorial.compute(0) != 1) {
+                throw new AssertionError("Test failed for input 0");
+            }
+	  
+            System.out.println("All tests passed.");
+        } catch (Exception e) {
+            System.err.println("Test failed: " + e.getMessage());
+            //System.exit(1); // non-zero exit will fail the Jenkins stage
+        }
+    }
+}
